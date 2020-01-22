4049d1cdeb00081b8c84ef71f1aa9d5bb425f0f3
rvrepo <rony.v12@gmail.com>
2020-01-22 23:42:52 +0530

    More changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 (function() {
 
   // Set the date we're counting down to
-  var countDownDate = new Date("Feb 28, 2020 21:00:00").getTime();
+  var countDownDate = new Date("Feb 1, 2020 21:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
